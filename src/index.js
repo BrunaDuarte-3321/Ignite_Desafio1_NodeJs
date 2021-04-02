93de3db66b01cbf73693170169a51703ce0e4170
Bruna Duarte <brunaduarte3000@hotmail.com>
2021-04-02 02:15:50 -0300

    Resolução do primeiro desafio do Bootcamp NodeJs

---
+++ src/index.js
@@ -0,0 +1,109 @@
+const express = require('express');
+const cors = require('cors');
+
+ const { v4: uuidv4 } = require('uuid');
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+ const users = [];
+
+function checksExistsUserAccount(request, response, next) {
+  const {username} = request.headers;
+
+  const validUsername = users.find((user) => user.username === username);
+
+  if(!validUsername){
+    return response.status(404).json({error: "Username not found!"});
+  }
+  request.user = validUsername;
+
+   return next();
+};
+
+function checkExistsTodo(request, response, next){
+
+  const {id} = request.params;
+  const {user} = request;
+
+  const todo = user.todos.find((todo) => todo.id === id)
+
+  if(!todo){
+    return response.status(404).json({error: "Todo not found"})
+  }
+
+  request.todo = todo;
+
+  return next();
+}
+
+app.post('/users', (request, response) => {
+  const {name, username} = request.body;
+
+  const validUsername = users.find(user => user.username === username);
+
+  if(validUsername){
+    return response.status(400).json({error: "Username exists!"})
+  }
+
+  const user = {
+    id: uuidv4(),
+    name,
+    username,
+    todos: [],
+  };
+
+  users.push(user);
+  return response.status(201).json(user)
+});
+
+app.get('/todos', checksExistsUserAccount, (request, response) => {
+   const {user} = request;
+   response.json(user.todos)
+});
+
+app.post('/todos', checksExistsUserAccount, (request, response) => {
+   const {user} = request;
+   const {title, deadline} = request.body;
+
+   const todo ={
+     id: uuidv4(),
+     title,
+     done: false,
+     deadline: new Date(deadline),
+     created_at: new Date(),
+   };
+
+   user.todos.push(todo);
+
+   return response.status(201).json(todo)
+});
+
+app.put('/todos/:id', checksExistsUserAccount,checkExistsTodo, (request, response) => {
+    const {todo, body: {title, deadline},} = request;
+    
+    todo.title = title;
+    todo.deadline = deadline;
+
+    return response.status(201).json(todo)
+});
+
+app.patch('/todos/:id/done', checksExistsUserAccount,checkExistsTodo, (request, response) => {
+   const {todo} = request;
+
+   todo.done = true;
+
+   return response.status(201).json(todo)
+});
+
+app.delete('/todos/:id', checksExistsUserAccount,checkExistsTodo, (request, response) => {
+   const {user, todo} = request;
+
+   user.todos.splice(todo.id, 1)
+
+   return response.status(204).json(user.todos);
+});
+
+module.exports = app;
